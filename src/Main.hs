67d7a6d644654c0deb535ade0b629afb306983ed
David Johnson <djohnson.m@gmail.com>
2016-06-27 00:58:31 -0500

    Updated event delegation code

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,43 @@
-{-# LANGUAGE ScopedTypeVariables      #-}
-{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
 
 module Main where
 
+import           Control.Concurrent
 import           Control.Monad
-import qualified Data.Foldable           as F
-import qualified Data.Text               as T
+import qualified Data.Foldable                 as F
+import           Data.List                     (find)
+import           Data.Maybe
+import           Data.Monoid
+import qualified Data.Text                     as T
 import           GHCJS.DOM
 import           GHCJS.DOM.CharacterData
-import           GHCJS.DOM.Document
+import           GHCJS.DOM.Document            hiding (drop)
+import           GHCJS.DOM.Event               (getType, Event, getTarget)
+import           GHCJS.DOM.EventTarget         (addEventListener)
+import           GHCJS.DOM.EventTargetClosures
 import           GHCJS.DOM.Node
-import           GHCJS.DOM.Types         hiding (Event)
+import           GHCJS.DOM.Types               hiding (Event)
+
+foreign import javascript unsafe "alert($1);" 
+  alert :: Double -> IO ()
+
+data Options = Options {
+       stopPropogation :: Bool
+     , preventDefault :: Bool
+     } deriving Show
+
+defaultOptions :: Options
+defaultOptions = Options False False
 
 data Attribute action 
-  = Event (String -> IO ())
+  = Event String (IO ())
   | KV Bool T.Text T.Text
 
-instance Show action => Show (Attribute action)
-  where show (Event _) = "<action>"
+instance Show action => Show (Attribute action) where
+  show (Event _ _) = "<action>"
+  show (KV _ k v) = T.unpack $ k <> "=" <> v
 
 type DOMNode = Maybe Node
 
@@ -26,32 +46,103 @@ data VTree action =
   | VText String DOMNode
   | VEmpty
 
+getChildDOMNodes :: VTree action -> [Node]
+getChildDOMNodes (VNode _ _ children _) = [ node | VNode _ _ _ (Just node) <- children ]
+getChildDOMNodes _ = []
+
+getDOMNode :: VTree action -> Maybe Node
+getDOMNode (VNode _ _ _ ref) = ref
+getDOMNode _ = Nothing
+
 instance Show action => Show (VTree action) where
   show VEmpty = "<empty>"
   show (VNode typ _ children _) =
     "<" ++ typ ++ ">" ++ concatMap show children ++ "</" ++ typ ++ ">"
   show (VText val _ ) = val
 
+mkNode :: String -> [Attribute action] -> [VTree action] -> VTree action
 mkNode name as cs = VNode name as cs Nothing
 
+text_ :: String -> VTree action
 text_ = flip VText Nothing
+
+div_ :: [Attribute action] -> [VTree action] -> VTree action
 div_  = mkNode "div"
+
+btn_ :: [Attribute action] -> [VTree action] -> VTree action
 btn_ = mkNode "button"
 
 click_ :: Attribute action 
-click_ = Event $ \_ -> putStrLn "hi"
+click_ = Event "click" (alert 42)
 
+delegate :: VTree a -> IO ()
+delegate vtree = do
+  Just doc <- currentDocument
+  Just body <- fmap toNode <$> getBody doc
+  listener <- eventListenerNew (f body)
+  addEventListener body ("click" :: String) (Just listener) True
+    where
+      f :: Node -> Event -> IO ()
+      f body e = do
+        (eventType :: String) <- getType e
+        Just target <- getTarget e
+        stack <- buildTargetToBody body (castToNode target)
+        delegateEvent vtree eventType stack
+
+buildTargetToBody :: Node -> Node -> IO [Node]
+buildTargetToBody body target = f target [target]
+    where
+      f currentNode nodes
+        | body == currentNode = pure (drop 2 nodes)
+        | otherwise = do
+            Just parent <- getParentNode currentNode
+            f parent (parent:nodes)
+
+delegateEvent :: VTree a -> String -> [Node] -> IO ()
+delegateEvent (VNode _ _ children _) eventName = findEvent children 
+    where
+      findEvent _ [] = pure ()
+      findEvent childNodes [y] = do
+       VNode _ attrs _ _ <- findNode childNodes y
+       forM_ (getAction attrs) $ \action -> action
+
+      findEvent childNodes (y:ys) = do
+        VNode _ _ childrenNext _ <- findNode childNodes y
+        findEvent childrenNext ys
+
+      findNode childNodes ref = do
+        let nodes = getVNodesOnly childNodes
+            Just x = flip find nodes $ \node ->
+              getDOMNode node == Just ref
+        pure x
+  
+      getVNodesOnly cs = do
+        vnode@VNode{} <- cs
+        pure vnode
+
+      getAction attrs =
+        listToMaybe $ do
+          Event evtName action <- attrs
+          guard (evtName == eventName)
+          pure action
+delegateEvent _ _ = const $ pure ()
+
+temp :: VTree ()
 temp = div_ [ ] [
     btn_ [ click_ ] [ text_ "hey" ]
   ]
-
+         
 main :: IO ()
 main = do
-  putStrLn "hi"
-  print =<< do
-    initTree (VNode "div" [] [ VNode "p" [] [ VText "heh2" Nothing ] Nothing ] Nothing)
-      :: IO (VTree ())
+  tree <- initTree temp
+  void $ forkIO $ delegate tree
+  forever $ do
+    threadDelay (secs 10)
+    pure ()
   
+secs :: Int -> Int
+secs = (*1000000)
+
 initTree :: Show action => VTree action -> IO (VTree action)
 initTree initial = do
   Just document <- currentDocument
@@ -131,7 +222,6 @@ go doc parent (VNode typA attrsA childrenA (Just ref)) (VNode typB attrsB childr
       VNode typB <$> diffAttrs doc ref attrsA attrsB
                  <*> diffChildren doc ref childrenA childrenB
                  <*> pure (Just ref)
-
 diffAttrs
   :: Show action
   => Document
@@ -139,7 +229,7 @@ diffAttrs
   -> [Attribute action]
   -> [Attribute action]
   -> IO [Attribute action]
-diffAttrs doc ref attrAs attrBs = pure []
+diffAttrs = undefined -- doc ref attrAs attrBs = pure []
   -- let as = S.fromList [ (k,v,o) | KV o k v <- attrAs ]
   --     bs = S.fromList [ (k,v,o) | KV o k v <- attrBs ]
   --     addAttrs = as `S.difference` bs
@@ -166,4 +256,3 @@ diffChildren doc parent (a:as) [] =
 diffChildren doc parent (a:as) (b:bs) = 
   (:) <$> go doc parent a b
       <*> diffChildren doc parent as bs
-
