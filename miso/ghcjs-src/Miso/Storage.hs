4cf910596bf6df26ed58f367d186a9b1980b6a70
David Johnson <djohnson.m@gmail.com>
2016-11-02 01:58:44 -0500

    Removed storage errors

--- miso/ghcjs-src/Miso/Storage.hs
+++ miso/ghcjs-src/Miso/Storage.hs
@@ -19,18 +19,27 @@ module Miso.Storage
   , sessionStorageLength
   ) where
 
-import           Data.Aeson
-import           Data.JSString
-import           Data.JSString.Text
-import           Data.String.Conversions
-import           Miso.Html.Internal
+import Data.Aeson              hiding (Object, String)
+import Data.JSString
+import Data.JSString.Text
+import Data.String.Conversions
+import GHCJS.Foreign
+import GHCJS.Types
+import Miso.Html.Internal
+import Unsafe.Coerce
 
 getLocalStorage, getSessionStorage ::
   FromJSON model => MisoString -> IO (Either String model)
-getLocalStorage key =
-   eitherDecode . cs . textFromJSString <$> getItemLS key
-getSessionStorage key =
-   eitherDecode . cs . textFromJSString <$> getItemSS key
+getStorageCommon
+  :: (Monad m, FromJSON b) => (t -> m JSVal) -> t -> m (Either String b)
+getStorageCommon f key = do
+  result <- f key
+  pure $ case jsTypeOf result of
+    Object -> Left "Not found"
+    String -> eitherDecode . cs $ textFromJSString (unsafeCoerce result)
+    _ -> Left "Unknown JS type, cannot decode"
+getSessionStorage = getStorageCommon getItemSS
+getLocalStorage = getStorageCommon getItemLS
 
 setLocalStorage, setSessionStorage ::
   ToJSON model => MisoString -> model -> IO ()
@@ -42,10 +51,10 @@ setSessionStorage key model = setItemSS key val
     val = textToJSString $ cs (encode model)
 
 foreign import javascript unsafe "$r = window.localStorage.getItem($1);"
-  getItemLS :: JSString -> IO JSString
+  getItemLS :: JSString -> IO JSVal
 
 foreign import javascript unsafe "$r = window.sessionStorage.getItem($1);"
-  getItemSS :: JSString -> IO JSString
+  getItemSS :: JSString -> IO JSVal
 
 foreign import javascript unsafe "window.localStorage.removeItem($1);"
   removeLocalStorage :: JSString -> IO ()
