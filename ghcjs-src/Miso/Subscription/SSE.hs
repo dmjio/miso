ae1ec73e42792e8093440aef5feb3fbee4d05013
David Johnson <djohnson.m@gmail.com>
2017-06-03 15:14:30 -0500

    Initial commit of SSE

---
+++ ghcjs-src/Miso/Subscription/SSE.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Miso.Subscription.SSE
+-- Copyright   :  (C) 2016-2017 David M. Johnson
+-- License     :  BSD3-style (see the file LICENSE)
+-- Maintainer  :  David M. Johnson <djohnson.m@gmail.com>
+-- Stability   :  experimental
+-- Portability :  non-portable
+----------------------------------------------------------------------------
+module Miso.Subscription.SSE where
+
+import GHCJS.Foreign.Callback
+import GHCJS.Types
+
+import Miso.String
+import Miso.Html.Internal     ( Sub )
+
+-- | Server-sent events Subscription
+sseSub :: MisoString -> (SSE -> action) -> Sub action model
+sseSub url f _ = \sink -> do
+  es <- newEventSource url
+  onMessage es =<< do
+    asyncCallback1 $ \val -> do
+      s <- SSEMessage <$> getData val
+      sink $ f s
+  onError es =<< do
+    asyncCallback $
+      sink (f SSEError)
+  onClose es =<< do
+    asyncCallback $
+      sink (f SSEClose)
+
+-- | Server-sent events data
+data SSE
+  = SSEMessage MisoString
+  | SSEClose
+  | SSEError
+  deriving (Show, Eq)
+
+foreign import javascript unsafe "$r = $1.data;"
+  getData :: JSVal -> IO JSString
+
+newtype EventSource = EventSource JSVal
+
+foreign import javascript unsafe "$r = new EventSource($1);"
+  newEventSource :: JSString -> IO EventSource
+
+foreign import javascript unsafe "$1.onmessage = $2;"
+  onMessage :: EventSource -> Callback (JSVal -> IO ()) -> IO ()
+
+foreign import javascript unsafe "$1.onerror = $2;"
+  onError :: EventSource -> Callback (IO ()) -> IO ()
+
+foreign import javascript unsafe "$1.onclose = $2;"
+  onClose :: EventSource -> Callback (IO ()) -> IO ()
+
+-- | Test URL
+-- http://sapid.sourceforge.net/ssetest/webkit.events.php
+-- var source = new EventSource("demo_sse.php");
+
