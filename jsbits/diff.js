a2e6b12c7611c7bd3e65a3ea66743c1c3efb36b0
David Johnson <djohnson.m@gmail.com>
2017-06-05 21:32:47 -0700

    Updated syncChildren algorithm

--- jsbits/diff.js
+++ jsbits/diff.js
@@ -5,22 +5,17 @@ function diff (currentObj, newObj, parent) {
   else if (currentObj && !newObj) parent.removeChild (currentObj.domRef);
   else {
     if (currentObj.type === "vtext") {
-      if (newObj.type === "vnode")
-        replaceElementWithText (currentObj, newObj, parent);
-      else
-        diffTextNodes (currentObj, newObj);
+      if (newObj.type === "vnode") replaceElementWithText (currentObj, newObj, parent);
+      else diffTextNodes (currentObj, newObj);
     } else {
-      if (newObj.type === "vnode")
-        diffVNodes (currentObj, newObj, parent);
-      else
-        replaceTextWithElement (currentObj, newObj, parent);
+      if (newObj.type === "vnode") diffVNodes (currentObj, newObj, parent);
+      else replaceTextWithElement (currentObj, newObj, parent);
     }
   }
 }
 
 function diffTextNodes (c, n) {
-  if (c.text !== n.text)
-    c.domRef.replaceData (0, c.domRef.length, n.text);
+  if (c.text !== n.text) c.domRef.replaceData (0, c.domRef.length, n.text);
   n.domRef = c.domRef;
 }
 
@@ -137,20 +132,238 @@ function hasKey (cs) {
 
 function diffChildren (cs, ns, parent) {
     var longest = ns.length > cs.length ? ns.length : cs.length;
-    for (var i = 0; i < longest; i++)
-      diff (cs[i], ns[i], parent);
+    if (hasKey(cs)) syncChildren (cs, ns, parent);
+    for (var i = 0; i < longest; i++) diff (cs[i], ns[i], parent);
 }
 
 function createElement (obj) {
-  obj.domRef = obj.ns === "svg"
-        ? document.createElementNS("http://www.w3.org/2000/svg", obj.tag)
+    obj.domRef = obj.ns === "svg"
+	? document.createElementNS("http://www.w3.org/2000/svg", obj.tag)
 	: document.createElement(obj.tag);
-  populate (null, obj);
+    populate (null, obj);
 }
 
 function createNode (obj, parent) {
-  if (obj.type === "vnode") createElement(obj);
-  else obj.domRef = document.createTextNode(obj.text);
-  parent.appendChild(obj.domRef);
+    if (obj.type === "vnode") createElement(obj);
+    else obj.domRef = document.createTextNode(obj.text);
+    parent.appendChild(obj.domRef);
 }
 
+function createNodeDontAppend (obj) {
+  if (obj.type === "vnode") createElement(obj);
+  else obj.domRef = document.createTextNode(obj.text);
+  return obj;
+}
+
+/* Child reconciliation algorithm, inspired by kivi and Bobril */
+function syncChildren (os, ns, parent) {
+    var oldFirstIndex = 0
+    , newFirstIndex = 0
+    , oldLastIndex = os.length - 1
+    , newLastIndex = ns.length - 1
+    , nFirst
+    , nLast
+    , oFirst
+    , oLast
+    , LIS;
+    for (;;) {
+	 /* check base case, first > last for both new and old
+	   [ ] -- old children empty (fully-swapped)
+	   [ ] -- new children empty (fully-swapped)
+	 */
+	if (newFirstIndex > newLastIndex && oldFirstIndex > oldLastIndex) break;
+	  nFirst = ns[newFirstIndex];
+	  nLast  = ns[newLastIndex];
+	  oFirst = os[oldFirstIndex];
+	  oLast  = os[oldLastIndex];
+	/* No more old nodes, create and insert all remaining nodes
+	   -> [ ] <- old children
+	   -> [ a b c ] <- new children
+	*/
+	if (oldFirstIndex > oldLastIndex) {
+	    var node = createNodeDontAppend(nFirst);
+	    /* insertBefore's semantics will append a node if the second argument provided is `null` or `undefined`.
+	       Otherwise, it will insert node.domRef before oLast.domRef.
+	     */
+	    parent.insertBefore(node.domRef, oLast ? oLast.domRef : null);
+	    newFirstIndex++;
+	    continue;
+	}
+	/* No more new nodes, delete all remaining nodes in old list
+	   -> [ a b c ] <- old children
+	   -> [ ] <- new children
+	*/
+	else if (newFirstIndex > newLastIndex) {
+	    parent.removeChild(oFirst.domRef);
+	    oldFirstIndex++;
+	    continue;
+	}
+	/* happy path, everything aligns, we continue
+	   -> oldFirstIndex -> [ a b c ] <- oldLastIndex
+	   -> newFirstIndex -> [ a b c ] <- newLastIndex
+	   check if nFirst and oFirst align, if so, check nLast and oLast
+	*/
+	else if (oFirst.key === nFirst.key) {
+	    nFirst.domRef = oFirst.domRef;
+	    newFirstIndex++;
+	    oldFirstIndex++;
+	    continue;
+	} else if (oLast.key === nLast.key) {
+	    oLast.domRef = nLast.domRef;
+	    newLastIndex--;
+	    oldLastIndex--;
+	    continue;
+	}
+	/* flip-flop case, nodes have been swapped, in some way or another
+	   both could have been swapped.
+	   -> [ a b c ] <- old children
+	   -> [ c b a ] <- new children */
+	else if (oFirst.key === nLast.key && nFirst.key === oLast.key) {
+	    var nextSib = oFirst.domRef.nextSibling;
+	    parent.insertBefore(oFirst.domRef, oLast.domRef);
+	    parent.insertBefore(nextSib, oLast.domRef);
+	    nFirst.domRef = oFirst.domRef;
+	    nLast.domRef = oLast.domRef;
+	    newFirstIndex++;
+	    oldFirstIndex++;
+	    oldLastIndex--;
+	    newLastIndex--;
+	    continue;
+	}
+
+	/* or just one could be swapped (d's align here)
+	   -> [ d b g ] <- old children
+	   -> [ a k d ] <- new children */
+	/* what do we do here? */
+	else if (oFirst.key === nLast.key) {
+	  continue;
+	}
+	/* what do we do here?
+	   on either side (e's align here)
+	   -> [ e b c ] <- old children
+	   -> [ b c e ] <- new children */
+	else if (oLast.key === nFirst.key) {
+	    continue;
+	}
+	/* The "you're screwed" case, nothing aligns, pull the ripcord, do something more fancy
+	   This can happen when the list is sorted, for example.
+	   -> [ a e c ] <- old children
+	   -> [ b e d ] <- new children
+	*/
+	else {
+	    /* Create array with length of new children list */
+	    var P = [], I = {}, i = 0, nLen = newLastIndex - newFirstIndex, oLen = oldLastIndex - oldFirstIndex
+	      , foundKey, last = -1, moved = false, newNodeIndex, removedNodes = 0;
+	    /* -1 means a new node should be inserted */
+	    for (i = pLen; i > 0; i--) P.append(-1);
+	    /* Create index I that maps keys with node positions of the remaining nodes from the new children */
+	    for (i = newFirstIndex; i <= newLastIndex; i++) I[ns[i].key] = i;
+	    /* Iterate over old nodes with Index, check if we can find node with same key in index */
+	    for (i = oldFirstIndex; i <= oldLastIndex; i++) {
+		node = os[i]; /* This will always return a match in this loop */
+		newNodeIndex = I[node.key];
+		/* If old node doesn't exist in new node list, remove it */
+		if (!newNodeIndex) {
+		    parent.removeChild(node.domRef);
+		    removedNodes++;
+		}
+		/* Found new node in index map */
+		else {
+		    /* Assign position of the node in the old children list to the positions array. */
+		    P[newNodeIndex] = i;
+		    /* When assigning positions in the positions array, we also keep the last seen node position of the new children
+		       list. If the last seen position is larger than current position of the node at the new list, then we are switching
+		       `moved` flag to `true`. */
+
+		    /* First check if last seen node position is larger than current node position */
+		    if (last > newNodeIndex) moved = true;
+		    /* Update last seen to  */
+		    last = newNodeIndex;
+		}
+	    }
+	    /* If `moved` flag is on, or if the length of the old children list minus the number of
+	       removed nodes isn't equal to the length of the new children list. Then go to the next step */
+	    if (moved /* DMJ: not sure we need this predicate ? ===> */ || (oLen - removedNodes !== nLen)) {
+		/* Find minimum number of moves if `moved` flag is on, or insert new nodes if the length is changed. */
+		LIS = lis(P);
+		lisIndex = LIS.length - 1;
+		while (nLen > -1, nLen--) {
+		    if (P[lisIndex] === nLen) {
+			listIndex--;
+			continue;
+		    }
+		    else if (LIS[lisIndex] !== nLen) {
+			/* node has moved */
+			ns[nLen].domRef = os[P[nLen]].domRef;
+			parent.insertBefore(ns[nLen].domRef, os[nLen].domRef);
+		    }
+
+
+		}
+	    } else if (!moved) {
+		/* When moved flag is off, we don't need to find LIS, and we just
+		   iterate over the new children list and check its
+		   current position in the positions array, if it is `-1`,
+		   then we insert new node. */
+		for (i = newFirstIndex; i <= newLastIndex; i++) {
+		    if (P[i] === -1) {
+			createNodeDontAppend(ns[i]);
+			/* Replace whatever is at current */
+			/* parent.replaceChild(ns[i].domRef, parent.children[i]); ... this doesn't seem right to me... */
+			parent.insertBefore(ns[i].domRef, parent.children[i+1]);
+		    }
+		}
+	    }
+	}
+    }
+}
+
+
+/* Thanks Boris :) */
+function lis(a) {
+  var p = a.slice(0);
+  var result = [], u, v, il, c, j;
+  result.push(0);
+
+  for (var i = 0, il = a.length; i < il; i++) {
+    if (a[i] === -1) {
+      continue;
+    }
+
+    j = result[result.length - 1];
+    if (a[j] < a[i]) {
+      p[i] = j;
+      result.push(i);
+      continue;
+    }
+
+    u = 0;
+    v = result.length - 1;
+
+    while (u < v) {
+      c = ((u + v) / 2) | 0;
+      if (a[result[c]] < a[i]) {
+	u = c + 1;
+      } else {
+	v = c;
+      }
+    }
+
+    if (a[i] < a[result[u]]) {
+      if (u > 0) {
+	p[i] = result[u - 1];
+      }
+      result[u] = i;
+    }
+  }
+
+  u = result.length;
+  v = result[u - 1];
+
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+
+  return result;
+}
