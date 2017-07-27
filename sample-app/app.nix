579159fdaa9fd34095ebf8b0e39c63f0c22458d0
David Johnson <djohnson.m@gmail.com>
2017-07-26 17:16:03 -0700

    Add quick start section

---
+++ sample-app/app.nix
@@ -0,0 +1,11 @@
+{ mkDerivation, base, miso, stdenv }:
+mkDerivation {
+  pname = "app";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [ base miso ];
+  description = "First miso app";
+  license = stdenv.lib.licenses.unfree;
+}
