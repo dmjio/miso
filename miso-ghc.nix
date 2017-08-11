d280526473f787cdc653cc1adf6cc315991a058d
David Johnson <djohnson.m@gmail.com>
2017-08-11 14:50:08 +0000

    Bump to 0.7.0.0

--- miso-ghc.nix
+++ miso-ghc.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "miso";
-  version = "0.6.0.0";
+  version = "0.7.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
--- miso-ghcjs.nix
+++ miso-ghcjs.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "miso";
-  version = "0.6.0.0";
+  version = "0.7.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
