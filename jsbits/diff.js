d61e03a640a9e78f78b762f4a5d4cd3226e1af05
David Johnson <djohnson.m@gmail.com>
2017-09-23 17:15:55 +0000

    Minor progress on syncChildren

--- jsbits/diff.js
+++ jsbits/diff.js
@@ -123,13 +123,16 @@ function diffCss (cCss, nCss, node) {
 }
 
 function hasKey (cs) {
-    return cs && cs[0] && cs[0].key;
+  return cs && cs[0] && cs[0].key;
 }
 
 function diffChildren (cs, ns, parent) {
     var longest = ns.length > cs.length ? ns.length : cs.length;
-    if (hasKey(cs)) syncChildren (cs, ns, parent);
-    for (var i = 0; i < longest; i++) diff (cs[i], ns[i], parent);
+    if (hasKey(cs))
+      syncChildren (cs, ns, parent);
+    else
+      for (var i = 0; i < longest; i++)
+        diff (cs[i], ns[i], parent);
 }
 
 function createElement (obj) {
@@ -159,17 +162,18 @@ function syncChildren (os, ns, parent) {
     , newLastIndex = ns.length - 1
     , nFirst
     , nLast
-    , oFirst
-    , oLast
-    , LIS
-    , temp;
+    , tmp
+    , found
+    , node;
     for (;;) {
 	 /* check base case, first > last for both new and old
 	   [ ] -- old children empty (fully-swapped)
 	   [ ] -- new children empty (fully-swapped)
 	 */
-	if (newFirstIndex > newLastIndex && oldFirstIndex > oldLastIndex)
+        console.log(os.map(function(x) { return x.key; }));
+	if (newFirstIndex > newLastIndex && oldFirstIndex > oldLastIndex) {
 	    break;
+	}
 
 	/* Initialize */
 	nFirst = ns[newFirstIndex];
@@ -182,25 +186,29 @@ function syncChildren (os, ns, parent) {
 	   -> [ a b c ] <- new children
 	*/
 	if (oldFirstIndex > oldLastIndex) {
-	    var node = createNodeDontAppend(nFirst);
+	    console.log ('oldFirstIndex > oldLastIndex');
+	    nFirst.domRef = document.createElement(nFirst.tag);
 	    /* insertBefore's semantics will append a node if the second argument provided is `null` or `undefined`.
 	       Otherwise, it will insert node.domRef before oLast.domRef. */
-	    parent.insertBefore(node.domRef, oLast ? oLast.domRef : null);
+	    parent.insertBefore(nFirst.domRef, oLast ? oLast.domRef : null);
 	    os.splice(newFirstIndex, 0, nFirst);
+            diff (null, nFirst, parent);
 	    newFirstIndex++;
-	    continue;
+            console.log(os.map(function(x) { return x.key; }));
 	}
 	/* No more new nodes, delete all remaining nodes in old list
 	   -> [ a b c ] <- old children
 	   -> [ ] <- new children
 	*/
 	else if (newFirstIndex > newLastIndex) {
+            console.log('newFirstIndex > newLastIndex');
 	    tmp = oldLastIndex - oldFirstIndex;
 	    while (tmp >= 0) {
 	      parent.removeChild(os[oldFirstIndex].domRef);
 	      os.splice(oldFirstIndex, 1);
 	      tmp--;
 	    }
+	    console.log(os.map(function(x) { return x.key; }));
 	    break;
 	}
 	/* happy path, everything aligns, we continue
@@ -209,27 +217,32 @@ function syncChildren (os, ns, parent) {
 	   check if nFirst and oFirst align, if so, check nLast and oLast
 	*/
 	else if (oFirst.key === nFirst.key) {
-	    newFirstIndex++;
-	    oldFirstIndex++;
-	    continue;
+            console.log('oFirst.key === nFirst.key');
+	    newFirstIndex++; oldFirstIndex++;
+            diff (oFirst, nFirst, parent);
+            console.log(os.map(function(x) { return x.key; }));
 	} else if (oLast.key === nLast.key) {
-	    newLastIndex--;
-	    oldLastIndex--;
-	    continue;
+            console.log('oLast.key === nLast.key');
+	    newLastIndex--; oldLastIndex--;
+            diff (oLast, nLast, parent);
+            console.log(os.map(function(x) { return x.key; }));
 	}
 	/* flip-flop case, nodes have been swapped, in some way or another
 	   both could have been swapped.
 	   -> [ a b c ] <- old children
 	   -> [ c b a ] <- new children */
 	else if (oFirst.key === nLast.key && nFirst.key === oLast.key) {
-	    var nextSib = oFirst.domRef.nextSibling;
+            console.log('oFirst.key === nLast.key && nFirst.key === oLast.key');
+	    node = oFirst.domRef.nextSibling;
 	    parent.insertBefore(oFirst.domRef, oLast.domRef);
-	    parent.insertBefore(nextSib, oLast.domRef);
+	    parent.insertBefore(node, oLast.domRef);
 	    newFirstIndex++;
 	    oldFirstIndex++;
 	    oldLastIndex--;
 	    newLastIndex--;
-	    continue;
+            diff (oLast, nLast, parent);
+            diff (oFirst, nFirst, parent);
+            console.log(os.map(function(x) { return x.key; }));
 	}
 	/* Or just one could be swapped (d's align here)
            This is top left and bottom right match case.
@@ -242,15 +255,15 @@ function syncChildren (os, ns, parent) {
 	   -> [ a b d ] <- new children
 	   and now we happy path
         */
-        else if (oFirst.key === nLast.key) {
-	    /* insertAfter */
-	    parentNode.insertBefore(oFirst.domRef, oLast.domRef.nextSibling);
-	    /* swap positions in old vdom */
-   	    os.splice(oFirstIndex, 0, os.splice(oLastIndex, 1)[0]);
-	    nLastIndex++;
-	    oLastIndex++;
-	    continue;
-	}
+        // else if (oFirst.key === nLast.key) {
+        //     console.log('oFirst.key === nLast.key');
+	//     /* insertAfter */
+	//     parent.insertBefore(oFirst.domRef, oLast.domRef.nextSibling);
+	//     /* swap positions in old vdom */
+   	//     os.splice(oldFirstIndex, 0, os.splice(oldLastIndex, 1)[0]);
+	//     newFirstIndex++;
+	//     oldFirstIndex++;
+	// }
 	/*
            This is top right and bottom lefts match case.
            We move d to end of list, mutate old vdom to reflect the change
@@ -261,15 +274,15 @@ function syncChildren (os, ns, parent) {
 	   -> [ a b d ] <- new children
 	   and now we happy path
 	*/
-        else if (oLast.key === nFirst.key) {
-	    /* insertAfter */
-	    parentNode.insertBefore(oLast.domRef, oFirst.domRef);
-	    /* swap positions in old vdom */
-	    os.splice(osLastIndex + 1, 0, os.splice(1, oFirstIndex)[0]);
-	    oFirstIndex++;
-	    nFirstIndex++;
-	    continue;
-	}
+        // else if (oLast.key === nFirst.key) {
+        //     console.log('oLast.key === nFirst.key');
+	//     /* insertAfter */
+	//     parent.insertBefore(oLast.domRef, oFirst.domRef);
+	//     /* swap positions in old vdom */
+	//     os.splice(oldLastIndex, 0, os.splice(1, oldFirstIndex)[0]);
+	//     oldFirstIndex++;
+	//     newFirstIndex++;
+	// }
 
 	/* The "you're screwed" case, nothing aligns, pull the ripcord, do something more fancy
 	   This can happen when the list is sorted, for example.
@@ -278,80 +291,65 @@ function syncChildren (os, ns, parent) {
 	*/
 
 	else {
-	    /* construct maps from key to index */
-	    var oMap = {}
-	    , nMap = {}
-	    , i = 0
-	    , n
-	    , o
-	    , idx
-	    , delta = 0;
+	    /* final case, perform linear search to check if new key exists in old map, decide what to do from there */
+	    found = false;
+	    tmp = oldFirstIndex;
+	    while (tmp <= oldLastIndex) {
+       	      if (os[tmp].key === nFirst.key) {
+		  found = true;
+		  node = os[tmp];
+		  break;
+	      }
+              tmp++;
+	    }
+	    /* If new key was found in old map this means it was moved, hypothetically as below
+	       -> [ a e b c ] <- old children
+	       -> [ b e a j ] <- new children
+                    ^
+	       In the above case 'b' has been moved, so we need to insert 'b' before 'a' in both vDOM and DOM
+	       We also increase oldFirstIndex and newFirstIndex.
 
-	    /* populate maps */
-	    for (i = newFirstIndex; i >= newLastIndex; i++)
-		nMap[ns[i].key] = i;
-	    for (i = oldFirstIndex; i >= oldLastIndex; i++)
-		oMap[os[i].key] = i;
+	       This results in new list below w/ updated index position
+	       -> [ b a e c ] <- old children
+	       -> [ b e a j ] <- new children
+                      ^
+   	    */
+	    if (found) {
+                console.log('found');
+                /* Move item to correct position */
+        	os.splice(oldFirstIndex, 0, os.splice(tmp, 1)[0]);
+		/* Swap DOM references */
+		parent.insertBefore(node.domRef, oFirst.domRef);
+		/* optionally perform `diff` here */
+                diff (os[oldFirstIndex], nFirst, parent);
+		/* increment counters */
+		oldFirstIndex++;
+		newFirstIndex++;
+                console.log(os.map(function(x) { return x.key; }));
+	    }
+	    /* If new key was *not* found in the old map this means it must now be created, example below
+	       -> [ a e d c ] <- old children
+	       -> [ b e a j ] <- new children
+                    ^
 
-	    /* Iterate over lists */
-	    while (newFirstIndex <= newLastIndex && oldFirstIndex <= oldLastIndex) {
+	       In the above case 'b' does not exist in the old map, so we create a new element and DOM reference.
+	       We then insertBefore in both vDOM and DOM.
 
-		/* Get current nodes */
-		n = ns[newFirstIndex];
-		o = ns[oldFirstIndex];
-
-		/* Base cases */
-		if (newFirstIndex > newLastIndex) {
-		    /* No more new nodes, delete the rest of the old nodes, continue looping */
-		    /* Remove from DOM */
-		    parent.removeChild(o.domRef);
-		    /* Remove from VDOM */
-		    os.splice(oldFirstIndex, 1);
-		    oldFirstIndex++;
-		    continue;
-		}
-
-		if (oldFirstIndex > oldLastIndex) {
-		    /* No more old nodes, add rest of new nodes to old child list, continue looping */
-		    /* Add to VDOM */
-		    tmp = createNodeDontAppend(n);
-		    os.splice(oldLastIndex + 1, 0, tmp);
-		    /* Add to DOM */
-		    parent.insertBefore(o.domRef, tmp);
-		    newFirstIndex++;
-		    continue;
-		}
-
-		/* Happy case, diff right here if you want */
-		if (n.key === o.key) {
-		    newFirstIndex++;
-		    newLastIndex++;
-		    continue;
-		}
-
-		/* Keys don't match, so check if new node exists in old child list */
-		idx = oMap[n.key];
-
-		/* If so, that means it has been moved, so we need to insert it into its correct location */
-		if (idx)
-		{
-		    /* swap oldFirstIndex with idx in os */
-		    /* append to DOM */
-		}
-                /* If it doesn't exist, that means we have a new node, and need to add it to old child list */
-		else
-		{
-		    tmp = createNodeDontAppend(n);
-		    os.splice(oldLastIndex + 1, 0, tmp);
-		    /* Add to DOM */
-		    parent.insertBefore(o.domRef, tmp);
-		    newFirstIndex++; /* Don't increment oldFirstIndex */
-		    continue;
-
-		}
+	       -> [ b a e d c ] <- old children
+	       -> [ b e a j   ] <- new children
+                      ^
+   	    */
+	    else {
+                console.log('not found');
+		createElement(nFirst);
+		parent.insertBefore(nFirst.domRef, oFirst.domRef);
+		os.splice(oldFirstIndex, 0, nFirst);
+		diff(oFirst,nFirst,parent);
+		newFirstIndex++;
+		oldFirstIndex++;
+		oldLastIndex++;
+		console.log(os.map(function(x) { return x.key; }));
 	    }
 	}
     }
 }
-
-
