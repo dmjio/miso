2dbf656820a516c142115a883b7cb08ace9b5540
David Johnson <djohnson.m@gmail.com>
2017-11-12 10:25:31 +0000

    Bump sample app hash

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -4,8 +4,8 @@ let
     owner = "dmjio";
     repo = "miso";
     sha256 = "0cv9hr35a3w02fzc79mvhinazdmxv9fp5ir4k4brhdarbnjxv98n";
-    rev = "82ae55f2cd81c6120fdfbb0bbe801a44a37bd282";
-  }) {};
+    rev = "91b7194c6e0baab3d367073449a8ada7ba8dc346";
+  }) { tests = false; };
 in pkgs.haskell.packages.ghcjs.callPackage ./app.nix {
   miso = result.miso-ghcjs;
 }
