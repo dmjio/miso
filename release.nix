038cacb981bfc31f671ec97278b8364e78eb68a7
David Johnson <djohnson.m@gmail.com>
2018-01-11 17:58:54 +0000

    Update release nix scripts

--- release.nix
+++ release.nix
@@ -1,8 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "a0aeb23";
+    sha256 = "04dgg0f2839c1kvlhc45hcksmjzr8a22q1bgfnrx71935ilxl33d";
+  }){}
+, tests ? false
+, haddock ? true
+}:
 let
   inherit (pkgs.haskell.packages) ghcjsHEAD;
-in { miso-release = import ./default.nix { nixpkgs = pkgs; };
+in { miso-release = import ./default.nix { inherit pkgs; };
      miso-ghcjs8 = ghcjsHEAD.callPackage ./miso-ghcjs.nix { };
-     haskell-miso.org = import ./examples/haskell-miso.org {};
-     sse-example = import ./examples/sse {};
+     haskell-miso.org = import ./examples/haskell-miso.org { inherit pkgs; };
+     sse-example = import ./examples/sse { inherit pkgs; };
    }
