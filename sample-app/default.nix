20a95dbccb46fd6a69ad37dc9c41e1652cb84c7f
David Johnson <djohnson.m@gmail.com>
2018-01-14 00:37:18 +0000

    Bump app hash

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -9,8 +9,8 @@ let
   result = import (pkgs.fetchFromGitHub {
     owner = "dmjio";
     repo = "miso";
-    sha256 = "18jhr1ihf0vwwvp134j24isvzq699x5iy7l9ihrah760zmcxi7d2";
-    rev = "e3d3d874337a4a44adc4b6bdb8b18d907c6c1e34";
+    sha256 = "03w1gffzw0qry6q5may5miyjf36574kx12pviywjddpckp62p8h3";
+    rev = "2a42c67ef1f876b2f81dbcf18bcc18269d0f6876";
   }) {};
 in pkgs.haskell.packages.ghcjs.callPackage ./app.nix {
   miso = result.miso-ghcjs;
