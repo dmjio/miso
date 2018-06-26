6f2d4a95a708ec9700498dbb8b5c0eb3598ce714
Jamie Ballingall <jamie.ballingall@gmail.com>
2018-06-26 13:50:11 -0400

    Conform all three copies of the sample/simple app to the one in /sample-app

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,29 +1,18 @@
+-- | Haskell language pragma
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+
+-- | Haskell module declaration
 module Main where
 
+-- | Miso framework import
 import Miso
 import Miso.String
 
+-- | Type synonym for an application model
 type Model = Int
 
-main :: IO ()
-main = startApp App { initialAction = SayHelloWorld, ..}
-  where
-    model  = 0
-    update = updateModel
-    view   = viewModel
-    events = defaultEvents
-    mountPoint = Nothing
-    subs   = []
-
-updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff (m + 1)
-updateModel SubtractOne m = noEff (m - 1)
-updateModel NoOp m = noEff m
-updateModel SayHelloWorld m = m <# do
-  putStrLn "Hello World!" >> pure NoOp
-
+-- | Sum type for application events
 data Action
   = AddOne
   | SubtractOne
@@ -31,10 +20,30 @@ data Action
   | SayHelloWorld
   deriving (Show, Eq)
 
-viewModel :: Int -> View Action
+-- | Entry point for a miso application
+main :: IO ()
+main = startApp App {..}
+  where
+    initialAction = SayHelloWorld -- initial action to be executed on application load
+    model  = 0                    -- initial model
+    update = updateModel          -- update function
+    view   = viewModel            -- view function
+    events = defaultEvents        -- default delegated events
+    subs   = []                   -- empty subscription list
+    mountPoint = Nothing          -- mount point for application (Nothing defaults to 'body')
+
+-- | Updates model, optionally introduces side effects
+updateModel :: Action -> Model -> Effect Action Model
+updateModel AddOne m = noEff (m + 1)
+updateModel SubtractOne m = noEff (m - 1)
+updateModel NoOp m = noEff m
+updateModel SayHelloWorld m = m <# do
+  putStrLn "Hello World" >> pure NoOp
+
+-- | Constructs a virtual DOM from a model
+viewModel :: Model -> View Action
 viewModel x = div_ [] [
    button_ [ onClick AddOne ] [ text "+" ]
- , text $ ms (show x)
+ , text (ms x)
  , button_ [ onClick SubtractOne ] [ text "-" ]
  ]
-
