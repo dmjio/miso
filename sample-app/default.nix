18dccdbf90dc8221b56f16e7bd3cd3fefa80b332
David Johnson <djohnson.m@gmail.com>
2019-07-14 17:23:16 -0400

    Remove stack, update README, update sample app

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -1,23 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-
-  pinnedPkgs = import (pkgs.fetchFromGitHub {
-    owner  = "NixOS";
-    repo   = "nixpkgs";
-    rev    = "4dd5c93998da55002fdec1c715c680531420381c";
-    sha256 = "06paxakic36nbdnwkkb1094fzp3lpzxxb1r57gmb3py6pb6xrcnh";
-  }){};
-
-  miso = pinnedPkgs.haskell.packages.ghcjs.callCabal2nix "miso" (pkgs.fetchFromGitHub {
-    owner  = "dmjio";
-    repo   = "miso";
-    rev    = "bb2be3264ff3c6aa3b18e471d7cf04296024059b";
-    sha256 = "07k1rlvl9g027fp2khl9kiwla4rcn9sv8v2dzm0rzf149aal93vn";
-  }){};
-
-in
-
-  pinnedPkgs.haskell.packages.ghcjs.callPackage ./app.nix {
-    inherit miso;
-  }
+with (import (builtins.fetchTarball {
+  url = "https://github.com/dmjio/miso/archive/b4c473f3ed6d6251ea7b8b489fc50076ac8d9b70.tar.gz";
+  sha256 = "11rby2s0hxbl28a4fcwdm9lcbjfysv862xd6b9jy0rgl63dh51i3";
+}) {});
+with pkgs.haskell.packages;
+ghcjs.callCabal2nix "app" ./. {}
