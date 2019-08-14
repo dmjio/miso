7dc8d5e1e609fb2aae2aacb771622b115134b1e8
David Johnson <djohnson.m@gmail.com>
2019-08-14 11:12:23 -0400

    Bump sample app hash.

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -1,5 +1,5 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/39b9e26ff41d6aab3b9d13a9d102ac56017f6a1f.tar.gz";
+  url = "https://github.com/dmjio/miso/archive/4e15d92cc4f37605fc398229695de4d903ef8a47.tar.gz";
   sha256 = "1lwr35p9074b7wgz0jh4f2pjc7ls8isgzmn9xl86vb6cvsm035kf";
 }) {});
 with pkgs.haskell.packages;
