adea0fa4a1d66f6a688538cc79d915c612fd38ad
David Johnson <djohnson.m@gmail.com>
2019-08-16 09:55:44 -0400

    Adjust sample app hash.

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -1,6 +1,6 @@
 with (import (builtins.fetchTarball {
   url = "https://github.com/dmjio/miso/archive/4e15d92cc4f37605fc398229695de4d903ef8a47.tar.gz";
-  sha256 = "1lwr35p9074b7wgz0jh4f2pjc7ls8isgzmn9xl86vb6cvsm035kf";
+  sha256 = "0kg9lg18qdsnb6gw3jmnl49widzf5cy151bls7k09d5dxlm0myd8";
 }) {});
 with pkgs.haskell.packages;
 ghcjs.callCabal2nix "app" ./. {}
