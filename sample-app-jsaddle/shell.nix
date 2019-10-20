e6c1deb8448b6bb80565682abf1f04fd4a53cd3b
Sridhar Ratnakumar <srid@srid.ca>
2019-10-20 11:42:50 -0400

    sample-app-jsaddle: Use cabal from nixpkgs
    This is required if the user does not have cabal installed globally.

--- sample-app-jsaddle/shell.nix
+++ sample-app-jsaddle/shell.nix
@@ -2,7 +2,7 @@ with (import ./default.nix);
 dev.env.overrideAttrs (old: {
   shellHook = ''
     function reload () {
-      ${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -T ':main'
+      ${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c '${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl' -T ':main'
     }
   '';
 })
