df52bf2fc8393bfa6f8c09434eccb36ac1aebd03
David Johnson <djohnson.m@gmail.com>
2019-11-15 18:53:32 -0500

    Some keys patch testing.

--- exe/Main.hs
+++ exe/Main.hs
@@ -8,7 +8,8 @@ module Main where
 
 -- | Miso framework import
 import Miso
-import Miso.String
+import Miso.String hiding (splitAt, reverse, length)
+import Data.List
 
 import Control.Monad.IO.Class
 
@@ -25,22 +26,26 @@ runApp = JSaddle.run 8080
 #endif
 
 -- | Type synonym for an application model
-type Model = Int
+data Model = Model { nums :: [Int], seed :: Int }
+  deriving (Eq, Show)
 
 -- | Sum type for application events
 data Action
-  = AddOne
-  | SubtractOne
+  = Before
+  | BeforeLast
+  | AfterFirst
+  | Clear
+  | After
+  | Middle
   | NoOp
-  | SayHelloWorld
   deriving (Show, Eq)
 
 -- | Entry point for a miso application
 main :: IO ()
 main = runApp $ startApp App {..}
   where
-    initialAction = SayHelloWorld -- initial action to be executed on application load
-    model  = 0                    -- initial model
+    initialAction = NoOp -- initial action to be executed on application load
+    model  = Model [1..10] 11      -- initial model
     update = updateModel          -- update function
     view   = viewModel            -- view function
     events = defaultEvents        -- default delegated events
@@ -49,16 +54,34 @@ main = runApp $ startApp App {..}
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff (m + 1)
-updateModel SubtractOne m = noEff (m - 1)
 updateModel NoOp m = noEff m
-updateModel SayHelloWorld m = m <# do
-  liftIO (putStrLn "Hello World") >> pure NoOp
+updateModel Clear m = noEff m { nums = [] }
+updateModel Before Model {..} = noEff $ Model (seed : nums) (seed + 1)
+updateModel After Model  {..}  = noEff $ Model (nums ++ [seed]) (seed + 1)
+updateModel Middle Model {..} = noEff $
+  case splitAt (length nums `div` 2) nums of
+    (ls,rs) ->
+      Model (ls ++ [seed] ++ rs) (seed + 1)
+updateModel BeforeLast Model {..} = noEff $
+  case reverse nums of
+    (z:zs) -> Model (reverse (z:seed:zs)) (seed + 1)
+updateModel AfterFirst Model {..} = noEff $
+  case nums of
+    (x:xs) ->
+      Model (x:seed:xs) (seed + 1)
 
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
-viewModel x = div_ [] [
-   button_ [ onClick AddOne ] [ text "+" ]
- , text (ms x)
- , button_ [ onClick SubtractOne ] [ text "-" ]
+viewModel m = div_ [] [
+   button_ [ onClick Clear ] [ text "Clear" ]
+ , button_ [ onClick Before ] [ text "Before" ]
+ , button_ [ onClick After ] [ text "After" ]
+ , button_ [ onClick BeforeLast ] [ text "Before Last" ]
+ , button_ [ onClick AfterFirst ] [ text "After First" ]
+ , button_ [ onClick Middle ] [ text "Middle" ]
+ , text $ ms $ "Next seed: " <> show (seed m)
+ , ul_ []
+   [ liKeyed_ (toKey x) [] [ text $ ms (show x) ]
+   | x <- nums m
+   ]
  ]
