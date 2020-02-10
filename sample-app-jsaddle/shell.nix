c70d860ca94d657af6f1eddf224cda20b0b8a8a4
Marek Fajkus <marek.faj@gmail.com>
2020-02-10 13:15:32 +0100

    better reload script

--- sample-app-jsaddle/shell.nix
+++ sample-app-jsaddle/shell.nix
@@ -1,9 +1,10 @@
 with (import ./default.nix);
-dev.env.overrideAttrs (old: {
-  shellHook = ''
-    function reload () {
-      ${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c '${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl' -T ':main'
-    }
-  '';
+let
+  reload-script = pkgs.writeScriptBin "reload" ''
+      ${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c \
+        '${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl' \
+        -T ':main'
+'';
+in dev.env.overrideAttrs (old: {
+  buildInputs = old.buildInputs ++ [reload-script];
 })
-
