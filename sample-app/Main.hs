a8691f315d7fd54fb382bf9c0be815d103fc398e
Adriano Melo <github@adrianomelo.com>
2020-10-03 00:27:18 +0200

    Typo in the sample-app (#626)

--- sample-app/Main.hs
+++ sample-app/Main.hs
@@ -31,7 +31,7 @@ main = startApp App {..}
     events = defaultEvents        -- default delegated events
     subs   = []                   -- empty subscription list
     mountPoint = Nothing          -- mount point for application (Nothing defaults to 'body')
-    logLevel = Off                -- used during prerendering to see if the VDOM and DOM are in synch (only used with `miso` function)
+    logLevel = Off                -- used during prerendering to see if the VDOM and DOM are in sync (only used with `miso` function)
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
