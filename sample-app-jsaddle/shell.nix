c831ea395bcc936dd04d065d69c10183e4005fe7
Sascha Heylik <github@heylik.com>
2021-09-08 15:22:26 +0200

    fix sample app .nix files for 1.7.1

--- sample-app-jsaddle/shell.nix
+++ sample-app-jsaddle/shell.nix
@@ -3,7 +3,7 @@ let
   reload-script = pkgs.writeScriptBin "reload" ''
       ${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c \
         '${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl' \
-        -T ':main'
+        -T ':run Main.main'
 '';
 in dev.env.overrideAttrs (old: {
   buildInputs = old.buildInputs ++ [reload-script];
