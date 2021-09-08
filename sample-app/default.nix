90f109872e794156b07e38458fc8ee8b0903749e
saschaheylik <42358327+saschaheylik@users.noreply.github.com>
2021-09-08 16:41:02 +0200

    fix sample app .nix files for 1.7.1 (#661)
    Co-authored-by: Sascha Heylik <github@heylik.com>

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -1,5 +1,4 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/ea25964565074e73d4052b56b60b6e101fa08bc5.tar.gz";
-  sha256 = "1yb9yvc0ln4yn1jk2k5kwwa1s32310abawz40yd8cqqkm1z7w6wg";
+  url = "https://github.com/dmjio/miso/archive/refs/tags/1.7.1.tar.gz";
 }) {});
 pkgs.haskell.packages.ghcjs.callCabal2nix "app" ./. {}
