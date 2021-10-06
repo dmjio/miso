a77a9c452f8e8b6ada4f60bb080f5bd67e3f9e27
David Johnson <djohnson.m@gmail.com>
2021-10-06 11:40:26 -0500

    Update deploy scripts.

--- examples/haskell-miso.org/nix/aws.nix
+++ examples/haskell-miso.org/nix/aws.nix
@@ -1,12 +1,178 @@
 { email ? ""
+, token ? ""
 }:
 let
   accessKeyId = "dev";
   region = "us-east-2";
   awsBox = { pkgs, config, lib, resources, ... }: {
-   imports = [ ./config.nix ];
-   _module.args = {
+   imports = [ ./module.nix ];
+   nixpkgs.config.packageOverrides = pkgs: {
+     haskell-miso = import ./../default.nix {};
+     misoPkgs = import ../../../default.nix { examples = true; };
+     coverage = import ../../../tests {};
+   };
+   security.acme = {
      inherit email;
+     acceptTerms = true;
+   };
+   services.haskell-miso.enable = true;
+   services.nginx = {
+     enable = true;
+     virtualHosts = {
+       "haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+             proxyPass = "http://127.0.0.1:3002";
+           };
+         };
+       };
+       "todo-mvc.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/todo-mvc.jsexe";
+           };
+         };
+       };
+       "coverage.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.coverage}/lcov-report";
+           };
+         };
+       };
+       "haddocks.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-ghcjs.doc}/share/doc/miso-1.8.0.0/html";
+           };
+         };
+       };
+       "flatris.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.pkgs.more-examples.flatris}/bin/app.jsexe";
+           };
+         };
+       };
+       "miso-plane.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = pkgs.misoPkgs.pkgs.more-examples.miso-plane;
+           };
+         };
+       };
+       "2048.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = pkgs.misoPkgs.pkgs.more-examples.the2048;
+           };
+         };
+       };
+       "threejs.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/threejs.jsexe";
+           };
+         };
+       };
+       "snake.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.pkgs.more-examples.snake}/bin/app.jsexe";
+           };
+         };
+       };
+       "router.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/router.jsexe";
+           };
+         };
+       };
+       "mario.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/mario.jsexe";
+           };
+         };
+       };
+       "simple.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-ghcjs}/bin/simple.jsexe";
+           };
+         };
+       };
+       "canvas.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/canvas2d.jsexe";
+           };
+         };
+       };
+       "svg.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/svg.jsexe";
+           };
+         };
+       };
+       "file-reader.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/file-reader.jsexe";
+           };
+         };
+       };
+       "xhr.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/xhr.jsexe";
+           };
+         };
+       };
+       "websocket.haskell-miso.org" = {
+          forceSSL = true;
+          enableACME = true;
+          locations = {
+          "/" = {
+            root = "${pkgs.misoPkgs.miso-examples}/bin/websocket.jsexe";
+           };
+         };
+       };
+     };
    };
    boot.loader.grub.device = pkgs.lib.mkForce "/dev/nvme0n1";
    networking.firewall = {
@@ -48,6 +214,7 @@ in
   resources.ec2KeyPairs.misoKeyPair = {
     inherit region accessKeyId;
   };
+
   inherit awsBox;
 
   network.enableRollback = true;
