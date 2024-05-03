c95686a675db6aa2c516d3ffd8ec406a06f1cdff
Samuel Evans-Powell <mail@sevanspowell.net>
2024-05-03 09:19:46 +0800

    threejs example: Use new JS backend syntax
    - Javascript conventions have changed slightly with the GHC Javascript
      backend (from GHCJS).
      - See https://github.com/ghcjs/ghcjs-base/pull/135/files.
    - Update foreign imports to match these new conventions, so compiling with GHC
      Javascript backend works.

--- examples/three/Main.hs
+++ examples/three/Main.hs
@@ -97,56 +97,56 @@ updateModel ref GetTime m = m <# do
 updateModel _ (SetTime m) _ =
   m <# pure GetTime
 
-foreign import javascript unsafe "$r = new Stats();"
+foreign import javascript unsafe "(() => { return new Stats(); })"
   newStats :: IO JSVal
 
-foreign import javascript unsafe "$1.begin();"
+foreign import javascript unsafe "((x) => { x.begin(); })"
   statsBegin :: JSVal -> IO ()
 
-foreign import javascript unsafe "$1.end();"
+foreign import javascript unsafe "((x) => { x.end(); })"
   statsEnd :: JSVal -> IO ()
 
-foreign import javascript unsafe "$1.showPanel(0);"
+foreign import javascript unsafe "((x) => { x.showPanel(0); })"
   showPanel :: JSVal -> IO ()
 
-foreign import javascript unsafe "$r = new THREE.Scene();"
+foreign import javascript unsafe "(() => { return new THREE.Scene();})"
   newScene :: IO JSVal
 
-foreign import javascript unsafe "$r = new THREE.BoxGeometry( $1, $2, $3 );"
+foreign import javascript unsafe "((x,y,z) => { return new THREE.BoxGeometry(x,y,z); })"
   newBoxGeometry :: Int -> Int -> Int -> IO JSVal
 
-foreign import javascript unsafe "$r = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );"
+foreign import javascript unsafe "(() => { return new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ); })"
   newCamera :: IO JSVal
 
-foreign import javascript unsafe "$r = new THREE.Mesh( $1, $2 );"
+foreign import javascript unsafe "((x,y) => { return new THREE.Mesh( x, y ); })"
   newMesh :: JSVal -> JSVal -> IO JSVal
 
-foreign import javascript unsafe "$r = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );"
+foreign import javascript unsafe "(() => { return new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); })"
   newMeshBasicMaterial :: IO JSVal
 
-foreign import javascript unsafe "$r = new THREE.WebGLRenderer({canvas:$1, antialias : true});"
+foreign import javascript unsafe "((x) => { return new THREE.WebGLRenderer({canvas:x, antialias : true}); })"
   newRenderer :: JSVal -> IO JSVal
 
-foreign import javascript unsafe "$1.setSize( window.innerWidth, window.innerHeight );"
+foreign import javascript unsafe "((x) => { x.setSize( window.innerWidth, window.innerHeight ); })"
   setSize :: JSVal -> IO ()
 
-foreign import javascript unsafe "$1.add($2);"
+foreign import javascript unsafe "((x, y) => { x.add(y); })"
   addToScene :: JSVal -> JSVal -> IO ()
 
-foreign import javascript unsafe "$1.position.z = $2;"
+foreign import javascript unsafe "((x, y) => { x.position.z = y; })"
   cameraZ :: JSVal -> Int -> IO ()
 
-foreign import javascript unsafe "$1.rotation.x += $2;"
+foreign import javascript unsafe "((a, y) => { a.rotation.x += y; })"
   rotateX :: JSVal -> Double -> IO ()
 
-foreign import javascript unsafe "$1.rotation.y += $2;"
+foreign import javascript unsafe "((x, a) => { x.rotation.y += a; })"
   rotateY :: JSVal -> Double -> IO ()
 
-foreign import javascript unsafe "$1.render($2, $3);"
+foreign import javascript unsafe "((x, y, z) => { x.render(y, z); })"
   render :: JSVal -> JSVal -> JSVal -> IO ()
 
-foreign import javascript unsafe "$1.position.z = $2;"
+foreign import javascript unsafe "((x, y) => { x.position.z = y; })"
   positionCamera :: JSVal -> Double -> IO ()
 
-foreign import javascript unsafe "$1.appendChild( $2.domElement );"
+foreign import javascript unsafe "((x, y) => { x.appendChild( y.domElement ); })"
   addStatsToDOM :: JSVal -> JSVal -> IO ()
