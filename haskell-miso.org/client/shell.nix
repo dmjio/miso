45a9be87072ad3854cbed43766109fe4b1b4b082
dmjio <code@dmj.io>
2025-02-20 15:48:59 -0600

    Add more CI build targets
    - Adds more CI jobs

--- haskell-miso.org/client/shell.nix
+++ haskell-miso.org/client/shell.nix
@@ -1,7 +1,2 @@
 with (import ../../default.nix {});
-let
-  inherit (pkgs.haskell.packages) ghcjs86;
-  src = import ../../nix/haskell/packages/source.nix pkgs;
-  client = ghcjs86.callCabal2nix "haskell-miso" (src.haskell-miso-src) {};
-in
-  client.env
+haskell-miso-client.env
