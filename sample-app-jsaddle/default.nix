60a56f7baad5fc0a4b9608551ef67401af59c35d
dmjio <code@dmj.io>
2025-02-21 15:47:11 -0600

    Add tagged releases to CI
    - Build tagged releases in CI to catch nix errors

--- sample-app-jsaddle/default.nix
+++ sample-app-jsaddle/default.nix
@@ -1,8 +1,10 @@
-with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/refs/tags/1.8.7.tar.gz";
-}) {});
+{ pkgSrc ? builtins.fetchTarball {
+    url = "https://github.com/dmjio/miso/archive/refs/tags/1.8.7.tar.gz";
+  }
+}:
+with (import pkgSrc {});
 {
-  dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. { inherit miso; };
+  dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. {};
   release = pkgs.haskell.packages.ghcjs86.callCabal2nix "app" ./. {};
   inherit pkgs;
 }
