bcf07e6e43d2f5c99088ec1c7c64c970878ca467
George Thomas <georgefsthomas@gmail.com>
2025-02-21 12:49:44 +0000

    Simplify target CPP
    The `J.run` in `jsaddle-warp` when using GHCJS is just `const id`, so we explicitly replicate that instead of importing anything.

--- src/Miso/Runner.hs
+++ src/Miso/Runner.hs
@@ -4,6 +4,7 @@ module Miso.Runner (run) where
 
 #if defined(wasm32_HOST_ARCH)
 import qualified Language.Javascript.JSaddle.Wasm as J
+#elif defined (ghcjs_HOST_OS)
 #else
 import qualified Language.Javascript.JSaddle.Warp as J
 import           System.Environment
@@ -12,16 +13,13 @@ import           System.Environment
 import           Language.Javascript.JSaddle
 
 -- | Entry point for a miso application
+run :: JSM () -> IO ()
 #if defined(wasm32_HOST_ARCH)
-run :: JSM () -> IO ()
 run = J.run
+#elif defined(ghcjs_HOST_OS)
+run = id
 #else
-run :: JSM () -> IO ()
-#ifndef ghcjs_HOST_OS
 run x = getProgName >>= \case
     "<interactive>" -> J.debug 8008 x
     _ -> J.run 8008 x
-#else
-run = J.run 8008
-#endif
 #endif
