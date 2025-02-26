44dbbbb491f8af698d9498ecb2ef7bbc1173578a
dmjio <code@dmj.io>
2025-02-25 21:08:57 -0600

    Remove 'mail', use 'notify'.

--- src/Miso.hs
+++ src/Miso.hs
@@ -22,7 +22,6 @@ module Miso
   , sink
   , notify
   , run
-  , mail
   , module Miso.Event
   , module Miso.Html
   , module Miso.Subscription
@@ -39,10 +38,7 @@ module Miso
 
 import           Control.Monad
 import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Class (lift)
-import           Control.Monad.Trans.Writer.Strict (tell)
 import           Data.IORef
-import qualified Data.Map.Strict as M
 import qualified JavaScript.Object.Internal as OI
 
 import           Miso.Diff
@@ -85,16 +81,3 @@ startApp app@App {..} = void $
     ref <- liftIO (newIORef vtree)
     registerSink app ref snk
     pure ref
-
--- | 'send' will send an 'action' to a different 'App'
--- Like 'notify' for 'Effect' interface, but doesn't require the model as an argument
-mail
-  :: App m a
-  -> a
-  -> Transition action model ()
-mail app action = lift $ tell [ \_ -> io ]
-  where
-    io = liftIO $ do
-      dispatch <- liftIO (readIORef componentMap)
-      forM_ (M.lookup (mountPoint app) dispatch) $ \(_, _, f) ->
-        f action
