b2fcd701069200ae0f2e67aa915dd9c23270c3be
dmjio <code@dmj.io>
2025-03-10 20:46:45 -0500

    Update shell environmnet

--- examples/sse/shell.nix
+++ examples/sse/shell.nix
@@ -1,11 +1,10 @@
 { pkg ? "ghc" }:
-
 with (import ./default.nix {});
 
-if pkg == "ghcjs"
-then sse-client.env
-else sse-server.env.overrideAttrs (d: {  
+if pkg == "ghc"
+then sse-server.env.overrideAttrs (d: {  
   shellHook = ''
-  alias runner="${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c 'cabal repl'"
+    alias runner="${pkgs.haskell.packages.ghc865.ghcid}/bin/ghcid -c 'cabal repl'"
   '';
 })
+else sse-client.env
