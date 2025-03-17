7a4cc8eb8a6cbc1b5339535d60cfa953e8508323
dmjio <code@dmj.io>
2025-03-17 18:08:33 -0500

    Use sample-app from top-level default.nix

--- sample-app/default.nix
+++ sample-app/default.nix
@@ -1,6 +1,6 @@
 with (import ../default.nix {});
 {
   inherit pkgs;
-  sample-app-js = pkgs.haskell.packages.ghcjs.callCabal2nix "app" ./. {};
-  sample-app = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. {};
+  inherit sample-app-js;
+  inherit sample-app;
 }
