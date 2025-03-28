383eb3095ee3d67428409398470dca669754f66d
dmjio <code@dmj.io>
2025-03-28 02:27:14 -0500

    Add jest support

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import jestPlugin from "eslint-plugin-jest";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, jestPlugin }, extends: ["js/recommended"] },
   { rules: {
       "no-global-assign": ["error", {"exceptions": ["module"]}]
 	},
