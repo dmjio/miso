2c8cf873cbb75d7de0f5b6e476919acc3e39782d
dmjio <code@dmj.io>
2025-03-28 01:51:57 -0500

    Add eslint to CI
    - Lint your JS
    - Apply recommendations

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+]);
