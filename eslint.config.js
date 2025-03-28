7b21042243a44d745c25355cba60ce5d9da4e427
dmjio <875324+dmjio@users.noreply.github.com>
2025-03-28 16:03:13 -0500

    Adds jest-environment-jsdom (#843)
    * Adds jest-environment-jsdom
    
    - Assume browser environmnet in miso.spec.js
    - Add beforeEach (() => document.body.innerHTML = '';) call
    - Drop explicit jsdom.JSDOM() references
    - Prettify new source
    - Update yarn.lock
    
    * Add arrow-body-style suggestion.

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,21 @@ import jestPlugin from "eslint-plugin-jest";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, jestPlugin }, extends: ["js/recommended"] },
-  { rules: {
-      "no-global-assign": ["error", {"exceptions": ["module"]}]
-	},
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, jestPlugin },
+    extends: ["js/recommended"],
+  },
+  {
+    rules: {
+      "no-global-assign": ["error", { exceptions: ["module"] }],
+      "arrow-body-style": ["error", "always"],
+    },
   },
 ]);
