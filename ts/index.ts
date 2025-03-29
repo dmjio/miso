8d12e051ac7d7d772b72f71b47d6fd61ca6e93c4
dmjio <code@dmj.io>
2025-03-29 13:48:22 -0500

    yarn -> bun, use TypeScript (miso.ts)
    - Port some of miso.js to TypeScript (miso.ts)
    - Use bun test runner (no more jsdom / jest)
    - Spruce up the .gitignore
    - Use bun.lock
    - Migrate tests to bun:test
    - Address tsc errors w/ miso.ts
    - Introduce index.ts (used to generate miso.js for Haskell build)
    - Use ESM syntax w/ miso.spec.js
    - Drop yarn.lock
    - Export top-level miso object namespace
    - Intrdouce .tsconfig

---
+++ ts/index.ts
@@ -0,0 +1,25 @@
+import
+{ diff
+, hydrate
+, version
+, delegate
+, callBlur
+, callFocus
+, eventJSON
+, undelegate
+, integrityCheck
+, setBodyComponent
+} from './miso';
+
+/* export globally */
+miso = {};
+miso['diff'] = diff;
+miso['hydrate'] = hydrate;
+miso['version'] = version;
+miso['delegate'] = delegate;
+miso['callBlur'] = callBlur;
+miso['callFocus'] = callFocus;
+miso['eventJSON'] = eventJSON;
+miso['undelegate'] = undelegate;
+miso['integrityCheck'] = integrityCheck;
+miso['setBodyComponent'] = setBodyComponent;
