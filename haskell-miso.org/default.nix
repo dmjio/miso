ef3285b32a77162dca82a11f228cfcaf75016bdb
dmjio <code@dmj.io>
2025-04-09 21:29:11 -0500

    src.haskell-miso-src -> sources.haskell-miso

--- haskell-miso.org/default.nix
+++ haskell-miso.org/default.nix
@@ -7,7 +7,7 @@ let
   ghcjs = legacyPkgs.haskell.packages.ghcjs;
   client = ghcjs.callCabal2nix "haskell-miso" (sources.haskell-miso) {};
   server = ghc.callCabal2nix "haskell-miso" (sources.haskell-miso) {};
-  dev = ghc.callCabal2nixWithOptions "haskell-miso" (src.haskell-miso-src) "-fdev" {};
+  dev = ghc.callCabal2nixWithOptions "haskell-miso" (sources.haskell-miso) "-fdev" {};
 in
 { haskell-miso-client = client;
   haskell-miso-server = server;
