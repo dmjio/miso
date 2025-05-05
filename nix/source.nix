1e0b0033f6e9b75d6193e26a6fa1a37685593650
dmjio <code@dmj.io>
2025-05-04 22:11:13 -0500

    Bump snake

--- nix/source.nix
+++ nix/source.nix
@@ -64,7 +64,7 @@ in
   snake = fetchFromGitHub {
     owner = "dmjio";
     repo = "miso-snake";
-    rev = "d26c0a3";
-    sha256 = "sha256-jD1kBqOMFrhxt+yUcXLTLL+qv71X4/uOI4qdkpO6nGA=";
+    rev = "b47396f61907ed95ee578c917b84e7ee2909134e";
+    sha256 = "sha256-q4DviMlqz31m8uqAO5HbdM6JyAyC9RJbn7gU+K+K+a8=";
   };
 }
