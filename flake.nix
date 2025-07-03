c2f1d1e29fa622d7aa7edbc8b5979426b51e9329
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-07-03 16:22:07 -0400

    nix: Switch from flake-utils to flake-parts (#1009)

--- flake.nix
+++ flake.nix
@@ -1,27 +1,33 @@
 {
   description = "A flake for miso";
 
-  inputs = {
+  nixConfig = {
+    extra-substituters = [
+      "https://haskell-miso-cachix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "haskell-miso-cachix.cachix.org-1:m8hN1cvFMJtYib4tj+06xkKt5ABMSGfe8W7s40x1kQ0="
+    ];
+  };
 
+  inputs = {
     nixpkgs.url =
       "https://github.com/alexfmpe/nixpkgs/archive/b594b289740a2bc917ed9c66fef5d905f389cb96.tar.gz";
 
-    flake-utils.url =
-      "github:numtide/flake-utils";
-
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, flake-utils }:
 
-   flake-utils.lib.eachSystem
-      [
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-darwin"
         "aarch64-linux"
         "x86_64-darwin"
-      ] (system:
+      ];
 
-      let
-        pkgs = import nixpkgs {
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [ (import ./nix/overlay.nix) ];
           config = {
@@ -30,24 +36,14 @@
           };
         };
 
-      in {
-        nixConfig = {
-          extra-substituters = [
-            "https://haskell-miso-cachix.cachix.org"
-          ];
-          extra-trusted-public-keys = [
-            "haskell-miso-cachix.cachix.org-1:m8hN1cvFMJtYib4tj+06xkKt5ABMSGfe8W7s40x1kQ0="
-          ];
-        };
         packages.default = pkgs.haskell.packages.ghc9122.miso;
 
-        devShells.default =
-          pkgs.mkShell {
-            name = "The miso ghc9122 shell";
-            buildInputs = with pkgs; [
-              pkgs.pkgsCross.ghcjs.haskell.packages.ghc9122.miso
-            ];
-          };
-      }
-   );
+        devShells.default = pkgs.mkShell {
+          name = "The miso ghc9122 shell";
+          buildInputs = with pkgs; [
+            pkgs.pkgsCross.ghcjs.haskell.packages.ghc9122.miso
+          ];
+        };
+      };
+    };
 }
