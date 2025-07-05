c42cc64b552cf097eae285b6ba5b5fb9d6166812
dmjio <code@dmj.io>
2025-07-05 17:30:03 -0500

    Update canvas example for three.js.

--- examples/three/Main.hs
+++ examples/three/Main.hs
@@ -1,213 +1,251 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE RecordWildCards            #-}
+-----------------------------------------------------------------------------
 {-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (C) 2016-2025 David M. Johnson
+-- License     :  BSD3-style (see the file LICENSE)
+-- Maintainer  :  David M. Johnson <code@dmj.io>
+-- Stability   :  experimental
+-- Portability :  non-portable
+-----------------------------------------------------------------------------
 module Main where
-
+-----------------------------------------------------------------------------
+import           GHC.Generics (Generic)
 import           Control.Monad
-import           Control.Monad.IO.Class (liftIO)
-import           Data.IORef
+import           Data.Function
 import           Language.Javascript.JSaddle
-
+-----------------------------------------------------------------------------
 import           Miso hiding ((<#))
+import qualified Miso.FFI as FFI
 import           Miso.String
 import           Miso.Style ((=:))
 import qualified Miso.Style as CSS
-
+-----------------------------------------------------------------------------
 #ifdef WASM
 foreign export javascript "hs_start" main :: IO ()
 #endif
-
+-----------------------------------------------------------------------------
 data Action
   = GetTime
-  | Init
   | SetTime !Double
-
-withStats :: Stats -> JSM () -> JSM ()
+-----------------------------------------------------------------------------
+withStats :: Stats -> Three () -> Three ()
 withStats stats action = do
-  statsBegin stats
+  stats & statsBegin
   action
-  statsEnd stats
-
+  stats & statsEnd 
+-----------------------------------------------------------------------------
 data Context = Context
-  { rotateCube :: JSM ()
-  , renderScene :: JSM ()
+  { cube :: Mesh
+  , scene :: Scene
   , stats :: Stats
-  }
-
-initContext :: IORef Context -> JSM ()
-initContext ref = do
-    canvas <- getElementById "canvas"
-    scene <- newScene
-    camera <- newCamera
-    renderer <- newRenderer canvas
-    setSize renderer
-    cube <-
-        join $
-            newMesh
-                <$> newBoxGeometry 1 1 1
-                <*> newMeshBasicMaterial
-    addToScene scene cube
-    positionCamera camera 5
-    stats <- newStats
-    statsContainer <- getElementById "stats"
-    addStatsToDOM statsContainer stats
-    liftIO $ writeIORef
-        ref
-        Context
-            { stats = stats
-            , rotateCube = do
-                rotateX cube 0.1
-                rotateY cube 0.1
-            , renderScene =
-                render renderer scene camera
-            }
-
-main :: IO ()
-main = run $ do
+  , renderer :: Renderer
+  , camera :: Camera
+  } deriving (Generic, FromJSVal, ToJSVal)
+-----------------------------------------------------------------------------
+initialize :: DOMRef -> Three Context
+initialize domRef = do
+  scene <- newScene
+  camera <- newCamera
+  renderer <- newRenderer domRef
+  renderer & setSize
+  geometry <- newBoxGeometry 1 1 1
+  basicMaterial <- newMeshBasicMaterial
+  cube <- newMesh geometry basicMaterial
+  (scene & addToScene) cube
+  positionCamera camera 5
   stats <- newStats
-  ref <- liftIO $ newIORef $ Context (pure ()) (pure ()) stats
-  m <- now
-  startComponent (component m (updateModel ref) viewModel)
-    { initialAction = Just Init
-    }
-
+  statsContainer <- getElementById "stats"
+  addStatsToDOM statsContainer stats
+  pure Context {..}
+-----------------------------------------------------------------------------
+draw :: Context -> Three ()
+draw Context {..} = withStats stats $ do
+  rotateX cube 0.1
+  rotateY cube 0.1
+  render renderer scene camera
+-----------------------------------------------------------------------------
+main :: IO ()
+main = run $ startComponent (component 0 updateModel viewModel)
+  { initialAction = Just GetTime
+  }
+-----------------------------------------------------------------------------
 viewModel :: Double -> View action
-viewModel _ =
-    div_
-        []
-        [ div_
-            [ id_ "stats"
-            , CSS.style_ [ "position" =: "absolute" ]
-            ]
-            []
-        , canvas_
-            [ id_ "canvas"
-            , width_ "400"
-            , height_ "300"
-            ]
-            []
-        ]
-
+viewModel _ = div_ []
+  [ div_
+    [ id_ "stats"
+    , CSS.style_ [ "position" =: "absolute" ]
+    ]
+    []
+  , canvas
+    [ width_ "400"
+    , height_ "300"
+    ]
+    initialize
+    draw
+  ]
+-----------------------------------------------------------------------------
 updateModel
-  :: IORef Context
-  -> Action
+  :: Action
   -> Effect Double Action
-updateModel ref Init = do
-  io $ do
-    initContext ref
-    pure GetTime
-updateModel ref GetTime = do
-  io $ do
-    Context{..} <- liftIO (readIORef ref)
-    withStats stats $ do
-      rotateCube
-      renderScene
-    SetTime <$> now
-updateModel _ (SetTime m) =
+updateModel GetTime =
+  io (SetTime <$> now)
+updateModel (SetTime m) =
   pure GetTime #> m
-
+-----------------------------------------------------------------------------
 newtype Stats = Stats { unStats :: JSVal }
-  deriving (MakeObject)
-
-newStats :: JSM Stats
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newStats :: Three Stats
 newStats = Stats <$> new (jsg @MisoString "Stats") ([] :: [MisoString])
-
+-----------------------------------------------------------------------------
 newtype Renderer = Renderer { unRenderer :: JSVal }
-  deriving (MakeObject)
-
-newRenderer :: JSVal -> JSM Renderer
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newRenderer :: JSVal -> Three Renderer
 newRenderer e = do
   renderer <- jsg ("THREE" :: MisoString) ! ("WebGLRenderer" :: MisoString)
   o <- create
   set "canvas" e o
   set "antialias" True o 
   Renderer <$> new renderer [o]
-
+-----------------------------------------------------------------------------
 newtype Scene = Scene { unScene :: JSVal }
-  deriving (MakeObject)
-
-newScene :: JSM Scene
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newScene :: Three Scene
 newScene = do
   scene <- jsg ("THREE" :: MisoString) ! ("Scene" :: MisoString)
   Scene <$> new scene ([] :: [MisoString])
-
+-----------------------------------------------------------------------------
 newtype BoxGeometry = BoxGeometry { unBoxGeometry :: JSVal }
-  deriving (MakeObject)
-
-newBoxGeometry :: Double -> Double -> Double -> JSM BoxGeometry
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newBoxGeometry :: Double -> Double -> Double -> Three BoxGeometry
 newBoxGeometry x y z = do
   boxGeometry <- jsg ("THREE" :: MisoString) ! ("BoxGeometry" :: MisoString)
   BoxGeometry <$> new boxGeometry [x,y,z]
-
+-----------------------------------------------------------------------------
 newtype Camera = Camera { unCamera :: JSVal }
-  deriving (MakeObject)
-
-newCamera :: JSM Camera
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newCamera :: Three Camera
 newCamera = do
   camera <- jsg ("THREE" :: MisoString) ! ("PerspectiveCamera" :: MisoString)
   Just w <- fromJSVal =<< jsg ("window" :: MisoString) ! ("innerWidth" :: MisoString)
   Just h <- fromJSVal =<< jsg ("window" :: MisoString) ! ("innerHeight" :: MisoString)
   Camera <$> new camera [75.0, w / h, 0.1, 1000 :: Double ]
-
+-----------------------------------------------------------------------------
 newtype Mesh = Mesh { unMesh :: JSVal }
-  deriving (ToJSVal, MakeObject)
-
-newMesh :: ToJSVal material => BoxGeometry -> material -> JSM Mesh
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newMesh :: ToJSVal material => BoxGeometry -> material -> Three Mesh
 newMesh (BoxGeometry box) m = do
   mesh <- jsg ("THREE" :: MisoString) ! ("Mesh" :: MisoString)
   material <- toJSVal m
   Mesh <$> new mesh [box, material]
-
+-----------------------------------------------------------------------------
 newtype MeshBasicMaterial = MeshBasicMaterial { unMeshBasicMaterial :: JSVal }
-  deriving (MakeObject, ToJSVal)
-
-newMeshBasicMaterial :: JSM MeshBasicMaterial
+  deriving newtype (MakeObject, ToJSVal)
+  deriving anyclass (FromJSVal)
+  deriving stock Generic
+-----------------------------------------------------------------------------
+newMeshBasicMaterial :: Three MeshBasicMaterial
 newMeshBasicMaterial = do
   material <- jsg ("THREE" :: MisoString) ! ("MeshBasicMaterial" :: MisoString)
   o <- create
   set "color" ("green" :: MisoString) o
   MeshBasicMaterial <$> new material [o]
-
-statsBegin :: Stats -> JSM ()
+-----------------------------------------------------------------------------
+statsBegin :: Stats -> Three ()
 statsBegin stats = void $ do
   stats # ("begin" :: MisoString) $ ([] :: [JSVal])
-
-statsEnd :: Stats -> JSM ()
-statsEnd stats = void $ do
-  stats # ("end" :: MisoString) $ ([] :: [JSVal])
-
-render :: Renderer -> Scene -> Camera -> JSM ()
+-----------------------------------------------------------------------------
+statsEnd :: Stats -> Three ()
+statsEnd stats = void $ stats # ("end" :: MisoString) $ ([] :: [JSVal])
+-----------------------------------------------------------------------------
+render :: Renderer -> Scene -> Camera -> Three ()
 render renderer (Scene scene) (Camera camera) = do
   void $ do renderer # ("render" :: MisoString) $ [scene, camera]
-
-addStatsToDOM :: JSVal -> Stats -> JSM ()
+-----------------------------------------------------------------------------
+addStatsToDOM :: JSVal -> Stats -> Three ()
 addStatsToDOM e stats = do
   child <- stats ! ("domElement" :: MisoString)
   void $ e # ("appendChild" :: MisoString) $ [child]
-
-setSize :: Renderer -> JSM ()
-setSize e = do
-  w <- jsg ("window" :: MisoString) ! ("innerWidth" :: MisoString)
-  h <- jsg ("window" :: MisoString) ! ("innerHeight" :: MisoString)
-  void $ e # ("setSize" :: MisoString) $ [w,h]
-
-addToScene :: Scene -> Mesh -> JSM ()
+-----------------------------------------------------------------------------
+setSize :: Renderer -> Three ()
+setSize renderer = do
+  w <- windowInnerWidth
+  h <- windowInnerHeight
+  void $ renderer # ("setSize" :: MisoString) $ [w,h]
+-----------------------------------------------------------------------------
+addToScene :: Scene -> Mesh -> Three ()
 addToScene x y = do
   void $ x # ("add" :: MisoString) $ [y]
-
-rotateY :: Mesh -> Double -> JSM ()
+-----------------------------------------------------------------------------
+rotateY :: Mesh -> Double -> Three ()
 rotateY mesh v = do
-  Just y <- fromJSVal =<< (mesh ! ("rotation" :: MisoString) ! ("y" :: MisoString))
+  y <- fromJSValUnchecked =<< (mesh ! ("rotation" :: MisoString) ! ("y" :: MisoString))
   void $ (mesh ! ("rotation" :: MisoString) <# ("y" :: MisoString)) (y + v)
-
-rotateX :: Mesh -> Double -> JSM ()
+-----------------------------------------------------------------------------
+rotateX :: Mesh -> Double -> Three ()
 rotateX mesh v = do
-  Just x <- fromJSVal =<< (mesh ! ("rotation" :: MisoString) ! ("x" :: MisoString))
+  x <- fromJSValUnchecked =<< (mesh ! ("rotation" :: MisoString) ! ("x" :: MisoString))
   void $ (mesh ! ("rotation" :: MisoString) <# ("x" :: MisoString)) (x + v)
+-----------------------------------------------------------------------------
+positionCamera :: Camera -> Double -> Three ()
+positionCamera camera = camera ! ("position" :: MisoString) <# ("z" :: MisoString)
+-----------------------------------------------------------------------------
+type Three = JSM
+-----------------------------------------------------------------------------
+-- | dmj: This belongs in its own library package called `miso-three`, using three.hs
+-- We'll keep this here for now since this example is based on an old three.js
+canvas
+  :: forall action canvasState
+   . (FromJSVal canvasState, ToJSVal canvasState)
+  => [ Attribute action ]
+  -> (DOMRef -> Three canvasState)
+  -- ^ Init function, takes 'DOMRef' as arg, returns canvas init. state.
+  -> (canvasState -> Three ())
+  -- ^ Callback to render graphics using this canvas' context, takes init state as arg.
+  -> View action
+canvas attributes initialize_ draw_ = node HTML "canvas" attrs []
+  where
+    attrs :: [ Attribute action ]
+    attrs = initCallback : drawCallack : attributes
 
-positionCamera :: Camera -> Double -> JSM ()
-positionCamera camera v = void $ do
-  (camera ! ("position" :: MisoString) <# ("z" :: MisoString)) v
+    initCallback :: Attribute action
+    initCallback = Event $ \_ o _ _ -> do
+      flip (FFI.set "onCreated") o =<< do
+        FFI.syncCallback1 $ \domRef -> do
+          initialState <- initialize_ domRef
+          FFI.set "state" initialState (Object domRef)
+
+    drawCallack :: Attribute action
+    drawCallack = Event $ \_ o _ _ -> do
+      flip (FFI.set "draw") o =<< do
+        FFI.syncCallback1 $ \domRef -> do
+          state <- fromJSValUnchecked =<< domRef ! ("state" :: MisoString)
+          draw_ state
+-----------------------------------------------------------------------------
