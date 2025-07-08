bd81e978d8e918ec39b76feaa84d7ceb3544ebd7
dmjio <code@dmj.io>
2025-07-08 12:46:43 -0500

    Bump nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -2,7 +2,7 @@ options:
 with (builtins.fromJSON (builtins.readFile ./nixpkgs.json));
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/alexfmpe/nixpkgs/archive/${rev}.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
   };
   config.allowUnfree = true;
