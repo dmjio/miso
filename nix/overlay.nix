a8fed1699d47147600d83f40f8a5e44e9daca431
dmjio <875324+dmjio@users.noreply.github.com>
2025-07-10 14:51:41 -0500

    Upstream `miso-lynx` GHCJS into `miso`. (#1026)
    - [x] This includes a BigInt patch to polyfill GHCJS
    - [x] `ghcNative` compiler + package set included
    - [x] Adds `native/` subdirectory included to miso's nix infra
    - [ ] Exposes from flake, with flake env.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -30,6 +30,9 @@ self: super: {
   # haskell stuff
   haskell = super.haskell // {
     packages = super.haskell.packages // {
+      ghcNative = super.haskell.packages.ghc9122.override {
+        overrides = import ./haskell/packages/native self;
+      };
       ghc9122 = super.haskell.packages.ghc9122.override {
         overrides = if super.stdenv.targetPlatform.isGhcjs
           then import ./haskell/packages/ghcjs self
