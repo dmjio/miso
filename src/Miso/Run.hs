e077247cf22cfa5fe5ce5f17dc12e677f77dcde3
David M. Johnson <875324+dmjio@users.noreply.github.com>
2025-10-31 17:02:50 -0500

    Use static file `Middleware` w/ `cabal run` (#1198)
    * Use `debugMiso` as default
    
    When using `cabal run` static file `Middleware` is not used. This PR makes `cabal repl` and `cabal run` both host static files from the current working directory. This is accomplished by making `debugMiso` the default entry point for `Miso.Run.run` w/ vanilla GHC.
    
    ```bash
    dmjios-MacBook-Pro:miso-todomvc dmjio$ cabal repl
    Configuration is affected by the following files:                                                                                                       - cabal.project
    Build profile: -w ghc-9.12.2 -O1
    In order, the following will be built (use -v for more details):
     - miso-todomvc-0.1 (interactive) (exe:app) (configuration changed)
    Configuring executable 'app' for miso-todomvc-0.1...
    Preprocessing executable 'app' for miso-todomvc-0.1...
    GHCi, version 9.12.2: https://www.haskell.org/ghc/  :? for help
    [1 of 2] Compiling Main             ( src/Main.hs, interpreted )
    Ok, one module loaded.
    ghci> main
    Running on port 8008...
    ghci> :! curl localhost:8008/cabal.project
    packages:                                                                                                                                                 .
    
    allow-newer:
      all:base
    
    flags: +template-haskell
    
    source-repository-package
      type: git
      location: https://github.com/dmjio/miso
      tag: 9e4806fc1b1736821122401a0bf47aae470f3033
    ```
    
    * Use static file `Middleware` w/ `cabal run`.

--- src/Miso/Run.hs
+++ src/Miso/Run.hs
@@ -44,12 +44,15 @@ run = J.run
 run = id
 #else
 run action = do
-    port <- fromMaybe 8008 . (readMaybe =<<) <$> lookupEnv "PORT"
-    isGhci <- (== "<interactive>") <$> getProgName
-    putStrLn $ "Running on port " <> show port <> "..."
-    if isGhci
-      then debugMiso port action
-      else J.run port action
+  port <- fromMaybe 8008 . (readMaybe =<<) <$> lookupEnv "PORT"
+  isGhci <- (== "<interactive>") <$> getProgName
+  putStrLn $ "Running on port " <> show port <> "..."
+  if isGhci
+    then debugMiso port action
+    else
+      runSettings (setPort port (setTimeout 3600 defaultSettings)) =<<
+        jsaddleOr defaultConnectionOptions (action >> syncPoint)
+        (static J.jsaddleApp)
 -----------------------------------------------------------------------------
 -- | Start or restart the server, with a static Middleware policy.
 --
