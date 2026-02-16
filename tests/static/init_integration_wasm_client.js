1919f700549807f4414d66d8d725f11acc00cd0a
Philipp Volguine <phil.volguine@gmail.com>
2026-02-16 09:20:38 -0500

    miso-tests: Temporary work-around to get wasm to load properly

--- tests/static/init_integration_wasm_client.js
+++ tests/static/init_integration_wasm_client.js
@@ -1,5 +1,5 @@
 import { WASI, OpenFile, File, ConsoleStdout } from "https://cdn.jsdelivr.net/npm/@bjorn3/browser_wasi_shim@0.3.0/dist/index.js";
-import ghc_wasm_jsffi from "./wasm.js";
+import make_ffi from "./wasm.js";
 
 const args = [];
 const env = ["GHCRTS=-H64m"];
@@ -12,11 +12,14 @@ const options = { debug: false };
 const wasi = new WASI(args, env, fds, options);
 
 const instance_exports = {};
+const jsffi = make_ffi(instance_exports);
+
 const { instance } = await WebAssembly.instantiateStreaming(fetch("/static/integration-client.wasm"), {
   wasi_snapshot_preview1: wasi.wasiImport,
-  ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
+  ghc_wasm_jsffi: jsffi,
 });
 Object.assign(instance_exports, instance.exports);
 
 wasi.initialize(instance);
+await jsffi.ZC0ZCmisozm1zi9zi0zi0zminplaceZCMisoZC();
 await instance.exports.hs_start(globalThis.example);
