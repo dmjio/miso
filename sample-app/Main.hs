8b3e87abaf7d9b08c3603f56430f35f26d1d985f
dmjio <code@dmj.io>
2026-02-20 17:48:12 -0600

    Add assets

--- sample-app/Main.hs
+++ sample-app/Main.hs
@@ -1,71 +1,134 @@
 ----------------------------------------------------------------------------
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE CPP               #-}
+{-# LANGUAGE CPP #-}
 ----------------------------------------------------------------------------
-module Main where
+import Miso
+    ( View
+    , App
+    , Effect
+    , component
+    , text
+    , Component
+    , toMisoString
+    , mount_
+    , io_
+    , consoleLog
+    , miso
+    , defaultEvents
+    )
 ----------------------------------------------------------------------------
-import           Miso
-import qualified Miso.Html as H
-import qualified Miso.Html.Property as P
-import           Miso.Lens
-----------------------------------------------------------------------------
--- | Component model state
-data Model
-  = Model
-  { _counter :: Int
-  } deriving (Show, Eq)
-----------------------------------------------------------------------------
-counter :: Lens Model Int
-counter = lens _counter $ \record field -> record { _counter = field }
-----------------------------------------------------------------------------
--- | Sum type for App events
-data Action
-  = AddOne
-  | SubtractOne
-  | SayHelloWorld
-  deriving (Show, Eq)
+import qualified Miso as M
+import qualified Miso.Html as M
+import qualified Miso.Html.Property as M
+import Miso.Lens (Lens, lens, (%=))
+import Miso.Binding ((-->), (<--))
+import GHC.Generics
+import Miso.JSON (FromJSON, ToJSON)
 ----------------------------------------------------------------------------
 -- | Entry point for a miso application
 main :: IO ()
-#ifdef INTERACTIVE
-main = reload (startApp defaultEvents app)
-#else
-main = startApp defaultEvents app
-#endif
+main = miso defaultEvents $ const $ rootApp 0
 ----------------------------------------------------------------------------
 -- | WASM export, required when compiling w/ the WASM backend.
 #ifdef WASM
-#ifndef INTERACTIVE
 foreign export javascript "hs_start" main :: IO ()
 #endif
-#endif
-----------------------------------------------------------------------------
--- | `component` takes as arguments the initial model, update function, view function
-app :: App Model Action
-app = component emptyModel updateModel viewModel
-----------------------------------------------------------------------------
--- | Empty application state
-emptyModel :: Model
-emptyModel = Model 0
-----------------------------------------------------------------------------
--- | Updates model, optionally introduces side effects
-updateModel :: Action -> Effect parent Model Action
-updateModel = \case
-  AddOne        -> counter += 1
-  SubtractOne   -> counter -= 1
-  SayHelloWorld -> io_ (consoleLog "Hello world")
-----------------------------------------------------------------------------
--- | Constructs a virtual DOM from a model
-viewModel :: Model -> View Model Action
-viewModel x =
-  H.div_
-    [ P.className "counter"
+
+type Action = ()
+
+data Model = Model
+    { valueA :: Int
+    , valueB :: Int
+    } deriving (Generic, ToJSON, FromJSON, Eq)
+
+
+valueALens :: Lens Model Int
+valueALens = lens valueA (\m x -> m { valueA = x })
+
+
+valueBLens :: Lens Model Int
+valueBLens = lens valueB (\m x -> m { valueB = x })
+
+
+type AppComponent = Component Model Model Action
+
+
+rootApp :: Int -> App Model Action
+rootApp depth =
+    (component initialModel update_ (rootView depth))
+        { M.logLevel = M.DebugAll }
+
+
+rootView :: Int -> Model -> View Model Action
+rootView depth m =
+    M.div_ []
+    (
+        M.button_
+            [ M.id_ "CLICKME"
+            , M.onClick ()
+            ]
+            [ text "Click Me" ]
+        : modelElems m
+        ++ [ mount_ $ innerApp depth ]
+    )
+
+
+innerApp :: Int -> AppComponent
+innerApp 0 =
+    (component initialModel update_ (view_ 0))
+        { M.logLevel = M.DebugAll
+        , M.bindings =
+            [ valueALens --> valueALens
+            , valueBLens <-- valueALens
+            ]
+        }
+
+innerApp idx =
+    (component initialModel update_ (view_ idx))
+        { M.logLevel = M.DebugAll
+        , M.bindings =
+            [ valueALens --> valueALens
+            , valueBLens <-- valueBLens
+            ]
+        }
+
+
+initialModel :: Model
+initialModel = Model 0 0
+
+
+update_ :: Action -> Effect a Model Action
+update_ = const $ do
+    io_ $ consoleLog "CLICKED"
+    valueALens %= (+ 1)
+
+
+view_ :: Int -> Model -> View Model Action
+view_ 0 m = M.div_ []
+    (
+        M.button_
+            [ M.onClick () ]
+            [ text "Clickme (innermost)" ]
+        :
+        modelElems m
+    )
+view idx m =
+    M.div_ []
+        ( modelElems m
+        ++
+        [ mount_ $ innerApp (idx - 1) ]
+        )
+
+
+modelElems :: Model -> [ View Model Action ]
+modelElems m =
+    [ M.div_ []
+        [ text (toMisoString $ show $ valueA m)
+        ]
+    , M.div_ []
+        [ text (toMisoString $ show $ valueB m)
+        ]
     ]
-    [ H.button_ [ H.onClick AddOne ] [ text "+" ]
-    , text $ ms (x ^. counter)
-    , H.button_ [ H.onClick SubtractOne ] [ text "-" ]
-    , H.br_ []
-    , H.button_ [ H.onClick SayHelloWorld ] [ text "Alert Hello World!" ]
-    ]
-----------------------------------------------------------------------------
+
