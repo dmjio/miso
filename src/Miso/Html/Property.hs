3252c7c0a6fe67656e4b5ceb67115e6b2c950c44
𝐧𝐞𝐫𝐬 <ners@gmx.ch>
2026-08-12 11:57:39 +0200

    fix: autocomplete, autocorrect, spellcheck attributes (#1575)

--- src/Miso/Html/Property.hs
+++ src/Miso/Html/Property.hs
@@ -256,9 +256,9 @@ acceptCharset_   = textProp "acceptCharset"
 action_ :: MisoString -> Attribute action
 action_  = textProp "action"
 -----------------------------------------------------------------------------
--- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autocomplete>
-autocomplete_ :: Bool -> Attribute action
-autocomplete_ b = textProp "autocomplete" (if b then "on" else "off")
+-- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/autocomplete>
+autocomplete_ :: MisoString -> Attribute action
+autocomplete_ = textProp "autocomplete"
 -----------------------------------------------------------------------------
 -- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autosave>
 autosave_ :: MisoString -> Attribute action
@@ -266,11 +266,11 @@ autosave_ = textProp "autosave"
 -----------------------------------------------------------------------------
 -- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autocorrect>
 autocorrect_ :: Bool -> Attribute action
-autocorrect_ b = textProp "autocomplete" (if b then "on" else "off")
+autocorrect_ b = textProp "autocorrect" (if b then "on" else "off")
 -----------------------------------------------------------------------------
 -- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/spellcheck>
 spellcheck_ :: Bool -> Attribute action
-spellcheck_ b = textProp "autocomplete" (if b then "on" else "off")
+spellcheck_ b = textProp "spellcheck" (if b then "true" else "false")
 -----------------------------------------------------------------------------
 -- | <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/role>
 role_ :: MisoString -> Attribute action
